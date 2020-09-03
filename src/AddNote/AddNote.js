5f584c6d558ec5f4b5e845406de2a463c9c4abb7
Richard Carmen Scott <RichardCarmenScott@gmail.com>
2020-09-03 15:15:48 -0400

    added textarea

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,65 +1,95 @@
-import React from 'react';
+import React from "react";
 import config from "../config";
 import ApiContext from "../ApiContext";
 
 export default class AddNote extends React.Component {
-    static contextType = ApiContext;
+  static contextType = ApiContext;
+  state = {
+    folderInput: "",
+  };
 
-    constructor(props) {
-        super(props);
-        this.nameInput = React.createRef();
-        this.folderInput = React.createRef();
-        this.nameContent = React.createRef();
-    };
+  constructor(props) {
+    super(props);
+    this.nameInput = React.createRef();
+    this.contentInput = React.createRef();
+  }
 
-    handleChange = (e) => {
-        console.log(this.folderInput)
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-        const name = this.nameInput.current.value;
-        //const id = this.folderInput.current.value
-        fetch(`${config.API_ENDPOINT}/notes`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({ 
-              name: name, 
-              //folderId: id,
-            }),
-        }).then((res) => {
-            return res.json();
-        })
-        .then((note) => {
-          this.context.addNote(note);
-          this.props.history.goBack();
-        });
-    };
+  handleChange = (e) => {
+    this.setState({ folderInput: e.target.value });
+  };
 
-    render() {
-        const { folders=[] } = this.context
-        const options = folders.map((folder, idx) => {
-            return (
-                <option ref={this.folderInput} value={folder.id} id={folder.id} key={idx}>
-                    {folder.name}
-                </option>
-            )
-        })
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.contentInput);
+    const name = this.nameInput.current.value;
+    const id = this.state.folderInput;
+    const content = this.contentInput.current.value;
+    fetch(`${config.API_ENDPOINT}/notes`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        folderId: id,
+        content: content,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((note) => {
+        this.context.addNote(note);
+        this.props.history.goBack();
+      });
+  }
 
+  render() {
+    const { folders = [] } = this.context;
+    const options = folders.map((folder, idx) => {
+      return (
+        <option
+          ref={this.folderInput}
+          value={folder.id}
+          id={folder.id}
+          key={idx}
+        >
+          {folder.name}
+        </option>
+      );
+    });
 
-        return (
-            <form className='AddNote' onSubmit={event => this.handleSubmit(event)}>
-                <label htmlFor='name'>
-                    <input type='text' name='name' className='AddNoteInput' ref={this.nameInput} />
-                </label>
-                <select name="folder" id="folders" aria-label='Folder Selections' onChange={(e) => this.handleChange(e)}>
-                {options} 
-                </select>  
-                <button type='submit' className='submitButton'>Submit</button>
-                
-            </form>
-        ); 
-    };
-};
+    return (
+      <form className="AddNote" onSubmit={(event) => this.handleSubmit(event)}>
+        <label htmlFor="name">
+          <input
+            type="text"
+            name="name"
+            className="AddNoteInput"
+            ref={this.nameInput}
+          />
+        </label>
+        <select
+          name="folder"
+          id="folders"
+          aria-label="Folder Selections"
+          onChange={(e) => this.handleChange(e)}
+        >
+          <option value={"null"}>Select Folder</option>
+          {options}
+        </select>
+        <label htmlFor="content">
+          <input
+            type="textarea"
+            name="content"
+            className="AddNoteContent"
+            ref={this.contentInput}
+          />
+        </label>
+        <button type="submit" className="submitButton">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
